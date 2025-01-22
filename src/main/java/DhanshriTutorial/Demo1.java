fc12999d1e894300a662ede4c97d855a521d9732
user <user@LAPTOP-FCOBB088>
2025-01-23 00:02:56 +0530

    Dhanshri Automation Test Case

---
+++ src/main/java/DhanshriTutorial/Demo1.java
@@ -0,0 +1,12 @@
+package DhanshriTutorial;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Dhanshri have written Automation test cases");
+		System.out.println("Dhanshri have written Automation test cases");
+		System.out.println("Dhanshri have written Automation test cases");
+
+	}
+}
